534e854639db694808d2059e8446d06e73ad4935
Misha Moroshko <michael.moroshko@gmail.com>
2021-03-25 16:47:49 +1100

    Finish implementation

--- src/RequestLetter.js
+++ src/RequestLetter.js
@@ -1,5 +1,41 @@
 /** @jsxImportSource @emotion/core */
-import { Container, Text, Header } from "basis";
+import {
+  Container,
+  Text,
+  Header,
+  DatePicker,
+  Footer,
+  Stack,
+  Form,
+  Input,
+  RadioGroup,
+  Button,
+} from "basis";
+
+const letterTypeOptions = [
+  {
+    label: "Credit limit decrease",
+    value: "Credit limit decrease",
+  },
+  {
+    label: "Account balance",
+    value: "Account balance",
+  },
+  {
+    label: "No activity letter",
+    value: "No activity letter",
+  },
+];
+
+const initialValues = {
+  letterType: "",
+  name: "",
+  dateOfBirth: {
+    day: "",
+    month: "",
+    year: "",
+  },
+};
 
 export default function RequestLetter() {
   return (
@@ -14,6 +50,38 @@ export default function RequestLetter() {
           </Text>
         </Container>
       </Container>
+      <Container hasBreakpointWidth padding="8 0">
+        <Form initialValues={initialValues}>
+          <Stack gap="8" width="100%" width-xs="400">
+            <RadioGroup
+              name="letterType"
+              label="Letter type"
+              columns={1}
+              options={letterTypeOptions}
+            />
+            <Input name="name" label="Name" />
+            <DatePicker name="dateOfBirth" label="Date of birth" />
+            <Button type="submit" width="100%" width-xs="200">
+              Request letter
+            </Button>
+          </Stack>
+        </Form>
+      </Container>
+      <Footer>
+        <Footer.Header>
+          <Footer.Header.Logo name="latitude" />
+          <Footer.Header.Social>
+            <Footer.Header.Social.Facebook
+              href="https://www.facebook.com/latitudefinancialservices/"
+              title="Latitude Financial Facebook"
+            />
+            <Footer.Header.Social.LinkedIn
+              href="https://www.linkedin.com/company/latitude-financial-services/"
+              title="Latitude Financial LinkedIn"
+            />
+          </Footer.Header.Social>
+        </Footer.Header>
+      </Footer>
     </>
   );
 }

c7d57b600b466f8f486cc246a475c82b24ef4289
Misha Moroshko <michael.moroshko@gmail.com>
2021-03-25 16:14:11 +1100

    Add header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import { Global, css } from "@emotion/core";
 import { BasisProvider, defaultTheme } from "basis";
 import RequestLetter from "./RequestLetter";
 import "@fontsource/montserrat";
+import "@fontsource/montserrat/600.css";
 import "@fontsource/roboto";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 export default function App() {
   return (
     <div>
+      <Global
+        styles={css`
+          body {
+            margin: 0;
+          }
+        `}
+      />
       <BasisProvider theme={defaultTheme}>
         <RequestLetter />
       </BasisProvider>
--- src/RequestLetter.js
+++ src/RequestLetter.js
@@ -1,10 +1,19 @@
 /** @jsxImportSource @emotion/core */
-import { Container, Text } from "basis";
+import { Container, Text, Header } from "basis";
 
 export default function RequestLetter() {
   return (
-    <Container hasBreakpointWidth>
-      <Text>Request a letter</Text>
-    </Container>
+    <>
+      <Header>
+        <Header.Logo name="latitude" />
+      </Header>
+      <Container bg="secondary.lightBlue.t15">
+        <Container hasBreakpointWidth padding="8 0">
+          <Text as="h1" textStyle="heading4" color="primary.blue.t100">
+            Request a letter
+          </Text>
+        </Container>
+      </Container>
+    </>
   );
 }
